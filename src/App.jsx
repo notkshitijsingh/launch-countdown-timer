e7e0c687fb49e6250cdc984e53faba5da3d7ab2a
alanb4rt <al4n.b4rt@gmail.com>
2024-10-18 14:16:24 +0200

    refacto tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,13 @@ export default function App() {
   return (
     <>
       <div className="flex flex-col w-full min-h-screen max-w-screen-md mx-auto p-8">
-        <div className="flex flex-col justify-between flex-1">
+        <div className="flex flex-col mt-12">
           <header>
             <h1 className="text-2xl md:text-3xl text-center tracking-[0.2em] font-bold uppercase py-16">
               We're launching soon
             </h1>
           </header>
-          <main className="flex-1">
+          <main>
             <CountdownSection />
           </main>
           <footer className="absolute bottom-0 inset-x-0 h-[20%]">
